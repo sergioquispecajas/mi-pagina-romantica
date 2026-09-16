08874a49a6548560889551709031020520f59476
Luis Angel <angelquispecajas@gmail.com>
2026-09-15 20:00:27 -0500

    Agregamos pantalla final y pequenos detalles

--- pagina.py
+++ pagina.py
@@ -10,9 +10,30 @@ def inicio():
 def fotos():
     return render_template("fotos.html")
 
+@app.route("/carta")
+def carta():
+    return render_template("carta.html")
+
 @app.route("/frases")
 def frases():
     return render_template("frases.html")
 
+@app.route("/contador")
+def contador():
+    return render_template("contador.html")
+
+@app.route("/musica")
+def musica():
+    return render_template("musica.html")
+
+@app.route("/final")
+def final():
+    return render_template("final.html")
+
+@app.route("/detalles")
+def detalles():
+    return render_template("detalles.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
+
